0c745079e4efebc96b960c72cf2a5786e23f3fc3
GerDevelop <121315492+Darkinsito@users.noreply.github.com>
2023-08-05 20:14:04 -0500

    realizado

--- Assets/Reto2D/Scripts/Player/PlayerRespawn.cs
+++ Assets/Reto2D/Scripts/Player/PlayerRespawn.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    public GameObject[] Heart;
+    private int life;
+
     private float checkPointPositionX, checkpointpositionY;
+
+    public Animator animator;
 
     void Start()
     {
+        life = Heart.Length;
+
         if (PlayerPrefs.GetFloat("checkPointPositionX")!=0)
         {
             transform.position = (new Vector2(PlayerPrefs.GetFloat("checkPointPositionX"), PlayerPrefs.GetFloat("checkPointPositionY")));
         }
     }
 
+    public void Checklife()
+    {
+        if (life < 1) 
+        {
+            Destroy(Heart[0].gameObject);
+            animator.Play("Trap");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        else if (life > 2)
+        {
+            Destroy(Heart[1].gameObject);
+            animator.Play("Trap");
+        }
+        else if(life < 3)
+        {
+            Destroy(Heart[2].gameObject);
+            animator.Play("Trap");
+        }
+    }
+    
     public void ReachedCheckPoint(float x, float y)
 
     {
@@ -22,5 +53,10 @@ public class PlayerRespawn : MonoBehaviour
         PlayerPrefs.SetFloat("checkPointPositionY",y);
     }
 
+    public void PlayerDamaged()
+    {
+        life--;
+        Checklife();
+    }
 
 }
